d05c56cca3343a7e64f9ae22122dac488650557d
Luke Goater <lukegoater@gmail.com>
2023-12-21 22:27:53 +0000

    feat: show prices on spaces when passing dates

--- app/Http/Resources/SpaceResource.php
+++ app/Http/Resources/SpaceResource.php
@@ -17,6 +17,11 @@ class SpaceResource extends JsonResource
         return [
             'id' => $this->id,
             'identifier' => $this->identifier,
+            'price' => $this->when($request->from, fn() => $this->reservations()->make([
+                    'from' => $request->from,
+                    'to' => $request->to,
+                ])->calculatePrice()
+            ),
             'created_at' => $this->created_at,
             'updated_at' => $this->updated_at,
         ];
